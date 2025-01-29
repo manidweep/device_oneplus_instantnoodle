4fd0cd810d16a95c7d56056aa186b2825e1009e1
Manidweep <manidweep678@gmail.com>
2025-01-29 13:12:02 -0500

    instantnoodle: Matrixx 11.1 Bringup

--- device.mk
+++ device.mk
@@ -30,7 +30,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-matrixx
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
